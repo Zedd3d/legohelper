93eea9a8ab6168b97fd860ae0ba4fa44e7d8c8df
Andrey <31339379+Zedd3d@users.noreply.github.com>
2026-04-19 18:54:36 +0300

    Include optional HTTP code in unknown errors

--- app/src/main/java/com/zeddikus/legohelper/di/ErrorTypes.kt
+++ app/src/main/java/com/zeddikus/legohelper/di/ErrorTypes.kt
@@ -3,7 +3,7 @@ package com.zeddikus.legohelper.di
 sealed interface ErrorTypes {
     data object NoNetwork: ErrorTypes
 
-    data object Unknown: ErrorTypes
+    data class Unknown(val code: Int? = null): ErrorTypes
 
     data object NoData: ErrorTypes
 }
